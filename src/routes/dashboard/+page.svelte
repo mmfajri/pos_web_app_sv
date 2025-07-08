654913bb832c0c75f9ca47e10a7450f30b7add3a
mmfajri <mmathlaiil.fajri@bni.co.id>
2025-07-08 16:33:47 +0700

    main : fixing eslint compiling error for svelte

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Welcome here!!! Main Menu</h1>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	let username: string = '';
+	let password: string = '';
+
+	function handleLogin(event: Event) {
+		event.preventDefault();
+		console.log('Logging in:', { username, password });
+
+		goto('/dashboard');
+	}
+</script>
+
+<h1>Login</h1>
+
+<form on:submit={handleLogin}>
+	<label>
+		Username :
+		<input type="text" bind:value={username} required />
+	</label>
+	<br />
+	<label>
+		Password :
+		<input type="password" bind:value={password} required />
+	</label>
+	<br />
+	<button type="submit">Log In</button>
+</form>
