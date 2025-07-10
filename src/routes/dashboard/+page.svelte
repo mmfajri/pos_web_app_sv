e59ecafee9176a7d26f48be079076886ab1877ea
mmfajri <mmathlaiil.fajri@bni.co.id>
2025-07-10 19:13:18 +0700

    main : setup tailwindcss

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Welcome here!!! Main Menu</h1>
+<h1 class="text-3xl font-bold underline">TEST !!!</h1>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	:global(html) {
+		background-color: theme(--color-gray-100);
+	}
+</style>
