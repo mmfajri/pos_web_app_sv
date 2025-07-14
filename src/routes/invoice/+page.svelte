0376edf26f33a58fd6bb63a8791ce1957466df89
mmfajri <mmathlailfajri@gmail.com>
2025-07-14 22:26:16 +0700

    main : finishing invoice page

--- src/routes/invoice/+page.svelte
+++ src/routes/invoice/+page.svelte
@@ -5,6 +5,7 @@
   import type { TransactionItem } from "$lib/models/TransactionItems";
   import type { Product } from "$lib/models/Product";
   import { TransactionController } from "$lib/controllers/TransactionController";
+  import { onMount } from "svelte";
 
   let invoiceDate = new SvelteDate();
 
@@ -31,6 +32,16 @@
     transactionController.updateQuantity(index, qty);
     items = transactionController.getItem();
   }
+
+  function removeItem(index: number) {
+    transactionController.removeItem(index);
+    items = transactionController.getItem();
+  }
+
+  // Sync to localStorage
+  // onMount(() => {
+  //  transactionController.load
+  // })
 
   $: subtotal = transactionController.getSubTotal().toFixed(2);
 </script>
@@ -59,17 +70,36 @@
           <th class="border px-2 py-1 text-right">Qty</th>
           <th class="border px-2 py-1 text-right">Price</th>
           <th class="border px-2 py-1 text-right">Amount</th>
+          <th class="border px-2 py-1 text-center">Action</th>
         </tr>
       </thead>
       <tbody>
-        {#each items as item}
+        {#each items as item, index}
           <tr>
             <td class="border px-2 py-1">{item}</td>
             <td class="border px-2 py-1">{item.code}</td>
             <td class="border px-2 py-1">{item.description}</td>
+            <td class="border px-2 py-1 text-right">
+              <input
+                type="number"
+                class="w-16 text-right border px-1 rounded"
+                min="1"
+                bind:value={item.quantity}
+                on:change={() => updateQty(index, item.quantity)}
+              />
+            </td>
             <td class="border px-2 py-1 text-right">{item.quantity}</td>
             <td class="border px-2 py-1 text-right">${item.price.toFixed(2)}</td>
             <td class="border px-2 py-1 text-right">${item.amount.toFixed(2)}</td>
+            <td class="border px-2 py-1 text-center">
+              <button
+                on:click={() => removeItem(index)}
+                class="text-red-600 font-semibold hover:underlined"
+                title="Remove"
+              >
+                X
+              </button>
+            </td>
           </tr>
         {/each}
       </tbody>
