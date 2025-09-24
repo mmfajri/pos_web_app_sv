d6a2851416ba5afb00c0e9146ecd1230c57a08b8
mmfajri <mmathlaiil.fajri@bni.co.id>
2025-09-24 15:58:35 +0700

    feat : add register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,40 +2,54 @@
   import { goto } from "$app/navigation";
 
   let username: string = "";
+  let email: string = "";
   let password: string = "";
+  let confirmPassword: string = "";
+
   let error: string | null = null;
   let loading = false;
 
-  function handleLogin(event: Event) {
+  function handleRegister(event: Event) {
     event.preventDefault();
     error = null;
     loading = true;
 
     // Simulate login
     setTimeout(() => {
-      console.log("Logging in:", { username, password });
-      goto("/dashboard");
+      console.log("Register Data", { username, password });
+      goto("/login");
     }, 1000);
   }
-  function handleRegister() {
-    goto("/register");
+
+  function handleLogin(event: Event) {
+    goto("/login");
   }
 </script>
 
 <div class="min-h-screen flex items-center justify-center bg-gray-100 px-4">
   <div class="w-full max-w-md bg-white p-8 rounded-2xl shadow-md">
-    <h1 class="text-2xl font-bold mb-6 text-center">Login</h1>
+    <h1 class="text-2xl font-bold mb-6 text-center">Register</h1>
 
     {#if error}
       <p class="text-red-500 text-sm mb-4">{error}</p>
     {/if}
 
-    <form on:submit={handleLogin} class="space-y-5">
+    <form on:submit={handleRegister} class="space-y-5">
       <div>
         <label class="block text-sm font-medium text-gray-700 mb-1">Username</label>
         <input
           type="text"
           bind:value={username}
+          required
+          class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
+      </div>
+
+      <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1">Email</label>
+        <input
+          type="text"
+          bind:value={email}
           required
           class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-400"
         />
@@ -51,14 +65,23 @@
         />
       </div>
 
+      <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1">Confirm Password</label>
+        <input
+          type="password"
+          bind:value={confirmPassword}
+          required
+          class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-400"
+        />
+      </div>
+
       <button
         type="submit"
         class="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-150 disabled:opacity-50"
         disabled={loading}
       >
-        {loading ? "Logging in..." : "Log In"}
+        {loading ? "Registering" : "Registered"}
       </button>
     </form>
-    <button on:click={handleRegister}>Dont Have Account ? Register here</button>
   </div>
 </div>
